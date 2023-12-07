a98db605860996e9951a17f61d4b687d4fcd8476
CloseCG <144056345+CloseCG@users.noreply.github.com>
2023-12-07 17:53:49 +0800

    [Enhancement] 渲染单词详情数据 #12 (#15)
    * feat:finish one of the static pages of test
    There will be three interfaces in total currently.
    
    * feat:Built three static intefaces of test module.
    
    - Modify part of the code of app.json and search.wxml.
    
    Fixes #7
    
    * feat:implement jump function and debug js files
    
    - Implement mutual jumps between three static interfaces.
    - Refactor the file structure of pages.
    
    * refactor:delete unnecessary files
    
    - Remove unnecessary files based on last commit.
    
    Fixes #11
    
    * feat: finish the design of the word details interface
    
    - Built the static interface of word details
    - Update a part of search interface.
    
    Fixes #10
    
    * feat: complete the data releated-part of word details
    
    - Successfully received the data from backend and render data to the interface.
    - Solved the bug that the path containing special charecters when jumping to a new page.
    
    Fixes #12
    
    ---------
    
    Co-authored-by: lzo <lzo@ll.com>

--- pages/search/search.js
+++ pages/search/search.js
@@ -3,7 +3,10 @@ Page({
   // Get the value of input bar
   searchInputWord(e){
     let that = this
-    let wordList = that.data.wordList
+    let historicalWordmark = 0 // historicalWordList mark
+    let searchedWordmark = 1 // searchedWordList mark
+    let searchedWordList = that.data.searchedWordList
+
     // Implement anti-shake processing of search box
     // Clear the delayer of coresponding timer
     clearTimeout(that.data.timer)
@@ -12,14 +15,14 @@ Page({
     that.setData({
       'timer': setTimeout(() => {
         let word = e.detail.value
-        wordList = [] // Clear old data
+        searchedWordList = [] // Clear old data
         // Determine whether it is empty
         if (word === ''){
-          that.setData({
-            wordList
-          })
+          that.updateDisplayedListMark(historicalWordmark)
+          that.updateWordAndDataList(that.data.historicalWordList, historicalWordmark)
           return
         }
+        that.updateDisplayedListMark(searchedWordmark)
         // Initiate a GET request to backend to obtain the information of words
         wx.request({
           url: 'http://ran.v1.idcfengye.com/search/slice',
@@ -30,20 +33,16 @@ Page({
           success: function(res) {
             // Render the information of words
             let code = res.data.code
-            if (code === 404){
-              that.setData({
-                wordList
-              })
+            if (code === 404){              
+              that.updateWordAndDataList(searchedWordList)
             } else if (code === 200){
               let wordObj = res.data.data
               let len = wordObj.length
               for (let i = 0; i < len; i++){
                 // Add the information of word
-                wordList.push(wordObj[i])
-                that.setData({
-                  wordList
-                })
-              }  
+                searchedWordList.push(wordObj[i])
+                that.updateWordAndDataList(searchedWordList, searchedWordmark)
+              }
             }
           },
         })
@@ -51,14 +50,97 @@ Page({
     })
   },
 
+  goToWord(e){
+    let that = this
+    let wordIndex = e.currentTarget.id.substring(5)
+    let wordInfom
+    let mark = this.data.displayedListMark
+    // According to mark choose one list
+    if (mark === 0){
+      wordInfom = this.data.historicalWordList[wordIndex]
+    } else if(mark === 1){
+      wordInfom = this.data.searchedWordList[wordIndex]
+    }
+    if (wordInfom === undefined) return;
+    // Replace special character
+
+    let word =  encodeURIComponent(wordInfom.word);
+    let wordJSON = JSON.stringify(word)
+    // // Local storage
+    // if (mark === 1){
+    //   // Add word to historical list
+    //   this.addElementToHistoricalList(wordInfom)
+    //   // Store data locally
+    //   wx.setStorage({
+    //     key: 'historicalWordList',
+    //     data: this.data.historicalWordList,
+    //     success () {
+    //       wx.getStorage({
+    //         key: 'historicalWordList',
+    //         success: (res) => {
+    //           that.updateDisplayedListMark(0)
+    //           console.log(res.data);
+    //           that.updateWordAndDataList(res.data, 0)
+    //         }
+    //       })
+    //     }
+    //   })
+    // }
+
+    wx.navigateTo({
+      url: '/pages/word/word?word=' + wordJSON,
+    })
+  },
+
+  updateWordAndDataList(newList, mark){
+    if (this.data.displayedListMark === mark){
+      this.setData({
+        wordList: newList,
+      })
+      if (mark === 0){
+        this.setData({
+          historicalWordList: newList,
+        })
+      } else if(mark === 1){
+        this.setData({
+          searchedWordList: newList,
+        })
+      }
+    }
+  },
+
+  updateDisplayedListMark(mark){
+    this.setData({
+      displayedListMark: mark
+    })
+  },
+  
+  // addElementToHistoricalList(e){
+  //   let list = this.data.historicalWordList
+  //   list.unshift(e)
+  //   this.setData({
+  //     historicalWordList: list
+  //   })},
+
   /**
    * 页面的初始数据
    */
   data: {
     // The time ID of Delay
     timer: null,
-    // Search result
-    wordList: []
+    // Displayed list
+    wordList: [],
+    // Searched word list
+    searchedWordList: [],
+    // Historical word list
+    historicalWordList: [{
+      id: '1',
+      word: 'word',
+      translation: 'n.单词'
+    }],
+    // An event lock for tow types of list who preferentially render data
+    // 0 presents historical list and 1 presents searched list
+    displayedListMark: 0
   },
 
   /**
